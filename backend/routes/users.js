d0e601d9efaf08a0aba7afbb4ee6270c85c39bb5
thaneeshbandi <thaneeshbandi@gmail.com>
2026-06-11 00:58:06 +0530

    Reorder profile view route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -34,8 +34,8 @@ router.use((req, res, next) => {
 router.post('/:username/follow', auth, followUser);
 router.get('/dashboard', auth, getUserDashboard);
 router.get('/suggested', auth, getSuggestedUsers);
-router.get('/profile/:username', optionalAuth, getUserProfileByUsername);
 router.post('/profile/:userId/view', auth, trackProfileView);
+router.get('/profile/:username', optionalAuth, getUserProfileByUsername);
 router.get('/me/feed', auth, getUserFeed);
 router.get('/me/stats', auth, getUserStats);
 router.get('/me/stories', auth, getUserStories);
